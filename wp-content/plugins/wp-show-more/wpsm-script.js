63011c98668611fcb08fce6841b81ab6ae76714e
zhijianer <475821273@qq.com>
2017-09-05 12:35:31 +0800

    wpsm-script

--- wp-content/plugins/wp-show-more/wpsm-script.js
+++ wp-content/plugins/wp-show-more/wpsm-script.js
@@ -1,13 +1,26 @@
-jQuery('.wpsm-content').addClass('wpsm-content-hide')
-jQuery('.wpsm-show, .wpsm-hide').removeClass('wpsm-content-hide')
-jQuery('.wpsm-show').on('click', function(e) {
-  jQuery(this).next('.wpsm-content').removeClass('wpsm-content-hide');
-  jQuery(this).addClass('wpsm-content-hide');
-  e.preventDefault();
-});
-jQuery('.wpsm-hide').on('click', function(e) {
-  var wpsm = jQuery(this).parent('.wpsm-content');
-  wpsm.addClass('wpsm-content-hide');
-  wpsm.prev('.wpsm-show').removeClass('wpsm-content-hide');
-  e.preventDefault();
-});
+var gw2_regression = null
+$('.wpsm-content').addClass('wpsm-content-hide')
+function gw2_Obj() {
+    $('.wpsm-show').on('click', function (e) {
+        var gw2_regression = document.body.scrollTop
+        $(this).next('.wpsm-content').show(1000)
+        $(this).hide()
+        e.preventDefault();
+        (function () {
+            gw2_one(gw2_regression)
+        })()
+    });
+    function gw2_one(num) {
+        gw2_regression = num
+        return gw2_regression
+    }
+
+    $('.wpsm-hide').on('click', function (e) {
+        var wpsm = $(this).parent('.wpsm-content');
+        wpsm.hide(1000)
+        wpsm.prev('.wpsm-show').show()
+        document.body.scrollTop = gw2_regression
+        e.preventDefault();
+    });
+}
+gw2_Obj()
